d933a7ecff5eb3a5b7e4d732c74c72a030c4181c
Gabi Kazav <gabik@google.com>
2016-10-25 14:34:56 +0300

    Shivam try

--- app/src/main/java/com/example/gabik/datepickertest/MainActivity.java
+++ app/src/main/java/com/example/gabik/datepickertest/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.gabik.datepickertest;
 
-import android.app.DialogFragment;
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.NumberPicker;
-
-import java.util.Calendar;
+import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,54 +17,37 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MyDatePicker newFragment = new MyDatePicker();
-                newFragment.show(getFragmentManager(), "MyDatePicker");
+                createDialogWithoutDateField().show();
             }
         });
+
+
+
     }
 
-    public static class MyDatePicker extends DialogFragment {
-
-        public interface EditNameDialogListener {
-            void onFinishEditDialog(String inputText);
-        }
-
-        public MyDatePicker() {
-            // Empty constructor required for DialogFragment
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            Calendar cc = Calendar.getInstance();
-            final int mon = cc.get(Calendar.MONTH) + 1;
-            final int year = cc.get(Calendar.YEAR);
-            View view = inflater.inflate(R.layout.custom_date_picker, container);
-            getDialog().setTitle("Please Choose Month and Year");
-
-            final NumberPicker mp = (NumberPicker) view.findViewById(R.id.monthPicker);
-            final NumberPicker yp = (NumberPicker) view.findViewById(R.id.yearPicker);
-            mp.setMaxValue(12);
-            mp.setMinValue(mon);
-            mp.setValue(mon);
-            mp.setWrapSelectorWheel(false);
-//            mp.setOnValueChangedListener(this);
-
-            yp.setMaxValue(year+1);
-            yp.setMinValue(year);
-            yp.setValue(year);
-            yp.setWrapSelectorWheel(false);
-            yp.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-                @Override
-                public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                    if (newVal == year) { mp.setMinValue(mon); }
-                    else {mp.setMinValue(1); mp.setValue(1);}
+    private DatePickerDialog createDialogWithoutDateField() {
+        DatePickerDialog dpd = new DatePickerDialog(this, null, 2014, 1, 24);
+        try {
+            java.lang.reflect.Field[] datePickerDialogFields = dpd.getClass().getDeclaredFields();
+            for (java.lang.reflect.Field datePickerDialogField : datePickerDialogFields) {
+                if (datePickerDialogField.getName().equals("mDatePicker")) {
+                    datePickerDialogField.setAccessible(true);
+                    DatePicker datePicker = (DatePicker) datePickerDialogField.get(dpd);
+                    java.lang.reflect.Field[] datePickerFields = datePickerDialogField.getType().getDeclaredFields();
+                    for (java.lang.reflect.Field datePickerField : datePickerFields) {
+                        Log.i("test", datePickerField.getName());
+                        if ("mDaySpinner".equals(datePickerField.getName())) {
+                            datePickerField.setAccessible(true);
+                            Object dayPicker = datePickerField.get(datePicker);
+                            ((View) dayPicker).setVisibility(View.GONE);
+                        }
+                    }
                 }
-            });
-
-            return view;
+            }
         }
-
+        catch (Exception ex) {
+        }
+        return dpd;
     }
 
 
