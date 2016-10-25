ae4cdbbe47a9b148cb3875828628ee0b75263d9b
Gabi Kazav <gabik@google.com>
2016-10-25 12:39:48 +0300

    test2

--- app/src/main/java/com/example/gabik/datepickertest/MainActivity.java
+++ app/src/main/java/com/example/gabik/datepickertest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.gabik.datepickertest;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,28 +11,37 @@ import android.widget.DatePicker;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new SelectDateFragmentTrans();
+                newFragment.show(getFragmentManager(), "DatePicker");
+            }
+        });
     }
 
-    public void datePick(View view) {
-        final Calendar c = Calendar.getInstance();
-        DatePickerDialog dpd = new DatePickerDialog(this, android.R.style.Theme_Holo_Dialog ,this,
-                c.get(Calendar.YEAR),
-                c.get(Calendar.MONTH),
-                c.get(Calendar.DAY_OF_MONTH));
+    public static class SelectDateFragmentTrans extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-        dpd.show();
-        //dpd.findViewById(Resources.getSystem().getIdentifier("day", "id", "android")).setVisibility(View.GONE);
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar calendar = Calendar.getInstance();
+            int yy = calendar.get(Calendar.YEAR);
+            int mm = calendar.get(Calendar.MONTH);
+            int dd = calendar.get(Calendar.DAY_OF_MONTH);
+            return new DatePickerDialog(getActivity(), this, yy, mm, dd);
+        }
+
+        public void onDateSet(DatePicker view, int yy, int mm, int dd) {
+        }
+
     }
 
-    @Override
-    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-
-    }
 
 }
